78ede98b64e01e73415bc7185f3e2d4a8a07d09d
SuBin Kim <hk6320042@gmail.com>
2026-05-10 00:40:50 +0900

    Modified Moniotr.cpp , Monitor.h

--- 2026_programming_class_project/Monitor.cpp
+++ 2026_programming_class_project/Monitor.cpp
@@ -1,11 +1,33 @@
 #pragma once
-#include <atomic>
+#include "Monitor.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
 
-// 다른 파일에서 접근 가능하도록 공유 변수 선언
-extern atomic<int> g_packetCount;
-extern atomic<long long> g_totalBytes;
+atomic<int> g_packetCount(0);
+atomic<long long> g_totalBytes(0);
+static bool g_keepRunning = true;
 
-// 모니터링 스레드 시작 함수
-void StartMonitoring();
-void StopMonitoring();
+void MonitorLoop() {
+    auto lastTime = chrono::steady_clock::now();
+    while (g_keepRunning) {
+        this_thread::sleep_for(chrono::seconds(1));
+
+        int currentCount = g_packetCount.exchange(0);
+        long long currentBytes = g_totalBytes.exchange(0);
+
+        cout << "\n[서버 부하 리포트]" << endl;
+        cout << " - PPS: " << currentCount << " pkts/s" << endl;
+        cout << " - Traffic: " << (currentBytes / 1024.0) << " KB/s" << endl;
+    }
+}
+
+void StartMonitoring() {
+    thread t(MonitorLoop);
+    t.detach(); // 독립적으로 실행
+}
+
+void StopMonitoring() {
+    g_keepRunning = false;
+}
--- 2026_programming_class_project/Monitor.h
+++ 2026_programming_class_project/Monitor.h
@@ -1,33 +1,11 @@
 #pragma once
-#include "Monitor.h"
-#include <iostream>
-#include <thread>
-#include <chrono>
+#include <atomic>
 using namespace std;
 
-atomic<int> g_packetCount(0);
-atomic<long long> g_totalBytes(0);
-static bool g_keepRunning = true;
+// 다른 파일에서 접근 가능하도록 공유 변수 선언
+extern atomic<int> g_packetCount;
+extern atomic<long long> g_totalBytes;
 
-void MonitorLoop() {
-    auto lastTime = chrono::steady_clock::now();
-    while (g_keepRunning) {
-        this_thread::sleep_for(chrono::seconds(1));
-
-        int currentCount = g_packetCount.exchange(0);
-        long long currentBytes = g_totalBytes.exchange(0);
-
-        cout << "\n[서버 부하 리포트]" << endl;
-        cout << " - PPS: " << currentCount << " pkts/s" << endl;
-        cout << " - Traffic: " << (currentBytes / 1024.0) << " KB/s" << endl;
-    }
-}
-
-void StartMonitoring() {
-    thread t(MonitorLoop);
-    t.detach(); // 독립적으로 실행
-}
-
-void StopMonitoring() {
-    g_keepRunning = false;
-}
+// 모니터링 스레드 시작 함수
+void StartMonitoring();
+void StopMonitoring();
